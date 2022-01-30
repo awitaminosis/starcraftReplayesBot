9724c65677536155518c8c97e9214c878e9f813a
awitaminosis <awitaminosis@mail.ru>
2022-01-30 10:49:38 +0300

    Procfile for heroku. debugging: Web process failed to bind to $PORT within 60 seconds of launch debug webhooks

--- run_bot.py
+++ run_bot.py
@@ -9,20 +9,27 @@ import os
 load_dotenv()
 
 
-async def on_startup(_):
+async def on_startup(dp):
     print("I'm online")
     await bot.set_webhook(os.getenv('URL_APP'))
+    print("1")
     sqlite_db.sql_start()
+    print("2")
 
 
-async def on_shutdown(_):
+async def on_shutdown(dp):
+    print("3")
     await bot.delete_webhook()
 
-
+print("4")
 client.register_handlers_client(dp)
+print("5")
 admin.register_handlers_admin(dp)
+print("6")
 admin_powers.register_handlers_admin_powers(dp)
+print("7")
 common.register_handlers_common(dp)
+print("8")
 
 # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 executor.start_webhook(
@@ -30,7 +37,8 @@ executor.start_webhook(
     webhook_path='',
     on_startup=on_startup,
     on_shutdown=on_shutdown,
-    skip_updates=False,
+    skip_updates=True,
     host='0.0.0.0',
     port=int(os.environ.get('PORT', 5000))
 )
+print("9")
