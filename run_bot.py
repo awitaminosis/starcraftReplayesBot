36bccb45de257083f1d3af8663cf5116483592b2
awitaminosis <awitaminosis@mail.ru>
2022-02-01 18:38:42 +0300

    Procfile for heroku. debug webhooks sleeping on heroku

--- run_bot.py
+++ run_bot.py
@@ -12,24 +12,16 @@ load_dotenv()
 async def on_startup(dp):
     print("I'm online")
     await bot.set_webhook(os.getenv('URL_APP'))
-    print("1")
     sqlite_db.sql_start()
-    print("2")
 
 
 async def on_shutdown(dp):
-    print("3")
     await bot.delete_webhook()
 
-print("4")
 client.register_handlers_client(dp)
-print("5")
 admin.register_handlers_admin(dp)
-print("6")
 admin_powers.register_handlers_admin_powers(dp)
-print("7")
 common.register_handlers_common(dp)
-print("8")
 
 # executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 executor.start_webhook(
@@ -37,8 +29,7 @@ executor.start_webhook(
     webhook_path='',
     on_startup=on_startup,
     on_shutdown=on_shutdown,
-    skip_updates=True,
+    skip_updates=False,
     host='0.0.0.0',
     port=int(os.environ.get('PORT', 5000))
 )
-print("9")
