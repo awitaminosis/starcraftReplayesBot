18da45d8c41fe4094fc9ee1e7bdaad133e11da9c
awitaminosis <awitaminosis@mail.ru>
2022-02-10 19:05:24 +0300

    only upload button for admin. deletion is better from bd

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -1,8 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 button_load = KeyboardButton("/upload")
-button_delete = KeyboardButton("/delete")
 
 button_case_admin = (
-    ReplyKeyboardMarkup(resize_keyboard=True).add(button_load).add(button_delete)
+    ReplyKeyboardMarkup(resize_keyboard=True).add(button_load)
 )
