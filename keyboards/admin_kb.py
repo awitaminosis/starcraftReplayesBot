f96e7925de7ac4924b292a46912e8246bb1fc624
awitaminosis <awitaminosis@mail.ru>
2022-02-10 19:29:36 +0300

    black

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -2,6 +2,4 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 button_load = KeyboardButton("/upload")
 
-button_case_admin = (
-    ReplyKeyboardMarkup(resize_keyboard=True).add(button_load)
-)
+button_case_admin = ReplyKeyboardMarkup(resize_keyboard=True).add(button_load)
